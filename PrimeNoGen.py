d3b01023e45a787eb5813370b9641196b1488062
Geekandi Tito <geekash254@gmail.com>
2017-01-17 12:41:52 +0300

    Prime no generator function accepting an input and giving an output of the prime no.s as a list

--- PrimeNoGen.py
+++ PrimeNoGen.py
@@ -1,7 +1,14 @@
-def prime_no_test():
-    n = 7
-    prime_in_range = []
-    for x in range(1,n):
-        if n%x == 0:
-            prime_in_range.append(n)
-        print(prime_in_range)
+# my prime number generator
+
+num = int(input("find prime numbers in which range:"))
+list_of_primes = []
+for x in range(2, num+1):
+    prime_no = True
+
+    for y in range(2,x):
+        if x % y == 0:
+            prime_no = False
+
+    if prime_no:
+        list_of_primes.append(x)
+print(list_of_primes)
